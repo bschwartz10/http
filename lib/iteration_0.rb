661a983dcb7612ff3c3b7c7dbaef8fade6e971e1
bschwartz10 <bschwartz92@gmail.com>
2017-02-13 17:49:58 -0700

    Add iteration_0

---
+++ lib/iteration_0.rb
@@ -0,0 +1,38 @@
+require 'socket'
+tcp_server = TCPServer.new(9292)
+
+puts "Ready for a request"
+
+counter = 1
+
+loop do
+
+  client = tcp_server.accept
+
+    request_lines = []
+
+    while line = client.gets and !line.chomp.empty?
+        request_lines << line.chomp
+    end
+
+    puts "Got this request:"
+    puts request_lines.inspect
+
+    puts "Sending response."
+
+    response = "<pre>" + "Hello World!" + " (#{counter})" + "</pre>"
+    output = "<html><head></head><body>#{response}</body></html>"
+    headers = ["http/1.1 200 ok",
+              "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+              "server: ruby",
+              "content-type: text/html; charset=iso-8859-1",
+              "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+    client.puts headers
+    client.puts output
+
+    puts ["Wrote this response:", headers, output].join("\n")
+
+    puts "\nResponse complete, exiting."
+    counter += 1
+end
+client.close
